17ef0f63e7205ace1f03e7c0f5a4117072a1d852
stlouisweb <stlouisweb@gmail.com>
2015-11-22 14:48:03 -0600

    Updates list_droplets method to get list of droplets instead of first one. Updates CLI method and REST endpoint.

--- index.py
+++ index.py
@@ -15,13 +15,16 @@ def droplet_create():
 
 # Returns the list of droplets(instances) on the account.
 def list_droplets():
-     token = '4e8073041c5811735cf470f95e8778e145eaa4515fe8cdb6ed15315be948ed8e'
-     droplets = requests.get('https://api.digitalocean.com/v2/droplets', auth=(token, ''))
+     token = "4e8073041c5811735cf470f95e8778e145eaa4515fe8cdb6ed15315be948ed8e"
+     droplets = requests.get("https://api.digitalocean.com/v2/droplets", auth=(token, ""))
      if droplets.status_code == 200:
-         droplets = droplets.json()
-         print 'DROPLETS:'
-         print(droplets['droplets'][0]["name"])
-         return droplets
+         do_droplets = droplets.json()
+         print "DROPLETS:"
+         do_droplets = do_droplets["droplets"]
+
+         for i, droplet in enumerate(do_droplets):
+             print(do_droplets[i]["name"])
+         return do_droplets
      return droplets.status_code
 
 
@@ -56,6 +59,6 @@ if __name__ == "__main__":
         elif ans !="":
           print("\n Not Valid Choice Try again")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import doctest
     doctest.testmod()
